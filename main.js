93fb783b55c0993a7befe063bf838b52c5cfda0b
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-06-30 19:50:53 +0530

    1st commit copyright to @aUgUsTiAnkUyAsHa

---
+++ main.js
@@ -0,0 +1,28 @@
+var Farewell = new Date('Jul 11, 2021 10:00:00').getTime();
+
+function counter(){
+    let dateCurrent =  new Date().getTime(),
+        timeLeft = Farewell - dateCurrent;
+     let second = 1000,
+        minute  = second * 60,
+        hour    = minute * 60,
+        day     = hour * 24;
+    let dayLeft     =  addZero(Math.floor(timeLeft/ day), 3),
+        hourLeft    =  addZero(Math.floor((timeLeft % day)/ hour), 2),
+        mintueLeft  =  addZero(Math.floor((timeLeft % hour)/minute), 2),
+        secondLeft  =  addZero(Math.floor((timeLeft % minute)/ second), 2);
+
+    function addZero (num, count) {
+        return num.toString().padStart(count, "0");
+    }
+
+    document.getElementById("day").innerHTML = dayLeft;
+    document.getElementById("hour").innerHTML = hourLeft;
+    document.getElementById("minute").innerHTML = mintueLeft;
+    document.getElementById("second").innerHTML = secondLeft;
+
+}
+
+setInterval(() => {
+    counter();
+}, 1000);
